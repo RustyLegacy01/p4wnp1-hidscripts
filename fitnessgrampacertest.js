b0dce899f419a39f0b7dbd545161d06c9965be38
Luca Noble <80450196+RustyLegacy01@users.noreply.github.com>
2021-03-16 13:15:52 +0100

    Create fitnessgrampacertest.js

---
+++ fitnessgrampacertest.js
@@ -0,0 +1,21 @@
+// Windows
+
+  
+layout('us');
+typingSpeed(1,1);
+press("GUI r");
+delay(500);
+type("notepad")
+press("ENTER")
+delay(500);
+type("The FitnessGram PACER Test is a multistage aerobic capacity test that progressively gets more difficult as it continues.")
+press("ENTER");
+type("The test is used to measure a student's aerobic capacity as part of the FitnessGram assessment. Students run back and forth as many times as they can, each lap signaled by a beep sound. The test get progressively faster as it continues until the student reaches their max lap score.")
+press("ENTER");
+type("The PACER Test score is combined in the FitnessGram software with scores for muscular strength, endurance, flexibility and body composition to determine whether a student is in the Healthy Fitness Zone™ or the Needs Improvement Zone™.")
+press("GUI r");
+delay(500);
+type("https://www.youtube.com/watch?v=Y82jDHRrswc");
+press("ENTER");
+delay(500);
+press("ALT TAB");
